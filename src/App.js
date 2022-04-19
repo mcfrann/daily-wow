281a88f1ca4a3a3f1c94f15b98ca3230f67fddd6
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-19 17:48:06 -0600

    Start basic structure for App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
+// import { Link, Route } from 'react-router-dom'
 
-function App() {
+const App = () => {
+  const [todaysWow, setTodaysWow] = useState(/*fetch random wow*/)
+  const [owenGraphich, setOwenGraphic] = useState(/*imported graphic */)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='App'>
+      <div className='page-container'>
+        <div className='owen-container'>
+          {/* <img className='owen-graphich' src=imported graphich alt="owen graphic" />  */}
+          {/*set state to image of owen w/ mouth closed. On click, state changes to mouth open, after timeout back to closed.*/}
+        </div>
+        <div className='about-container'>
+          <h1 className='about-header'>about today's wow:</h1>
+          <div className='about-text-container'>
+            <p className='about-text'>
+              Today's wow was featured in the movie 'Zoolander'. And other
+              interesting things.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App

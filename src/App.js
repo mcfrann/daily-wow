0d070e2a12904aa25243def4bdbb8c02ad6d8290
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-20 11:59:16 -0600

    route to home page component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import './App.css'
 import { useState, useEffect } from 'react'
-// import { Link, Route } from 'react-router-dom'
+import HomePage from './Components/HomePage/HomePage'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   const [todaysWow, setTodaysWow] = useState(/*fetch random wow*/)
-  const [owenGraphich, setOwenGraphic] = useState(/*imported graphic */)
+  const [owenGraphic, setOwenGraphic] = useState(/*imported graphic */)
 
   return (
     <section className='App'>
-      <div className='page-container'>
-        <div className='owen-container'>
-          {/* <img className='owen-graphich' src=imported graphich alt="owen graphic" />  */}
-          {/*set state to image of owen w/ mouth closed. On click, state changes to mouth open, after timeout back to closed.*/}
-        </div>
-        <div className='about-container'>
-          <h1 className='about-header'>about today's wow:</h1>
-          <div className='about-text-container'>
-            <p className='about-text'>
-              Today's wow was featured in the movie 'Zoolander'. And other
-              interesting things.
-            </p>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+      </Routes>
     </section>
   )
 }

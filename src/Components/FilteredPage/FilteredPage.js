684c187f1d9048b31730bc7a6965830581bd9313
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-20 18:24:24 -0600

    construct filtered component

---
+++ src/Components/FilteredPage/FilteredPage.js
@@ -0,0 +1,17 @@
+import './FilteredPage.css'
+import Tile from '../Tile/Tile'
+
+const FilteredPage = ({ filteredWows }) => {
+  // const filtered = favoriteWows.map((wow) => (
+  //   <Tile
+  //     key={Date.now()}
+  //     movie={wow.movie}
+  //     currentWow={wow.current_wow_in_movie}
+  //     audio={wow.audio}
+  //   />
+  // ))
+
+  return <div className='filtered-container'>hellooo</div>
+}
+
+export default FilteredPage

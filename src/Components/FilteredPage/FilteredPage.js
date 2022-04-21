1c29e7e6e8375255ece1f5c54e0607fa67f9bca7
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-20 19:32:00 -0600

    Started imported filtered list

--- src/Components/FilteredPage/FilteredPage.js
+++ src/Components/FilteredPage/FilteredPage.js
@@ -2,16 +2,16 @@ import './FilteredPage.css'
 import Tile from '../Tile/Tile'
 
 const FilteredPage = ({ filteredWows }) => {
-  // const filtered = favoriteWows.map((wow) => (
-  //   <Tile
-  //     key={Date.now()}
-  //     movie={wow.movie}
-  //     currentWow={wow.current_wow_in_movie}
-  //     audio={wow.audio}
-  //   />
-  // ))
+  const filtered = filteredWows.map((wow) => (
+    <Tile
+      key={Date.now()}
+      movie={wow.movie}
+      currentWow={wow.current_wow_in_movie}
+      audio={wow.audio}
+    />
+  ))
 
-  return <div className='filtered-container'>hellooo</div>
+  return <div className='filtered-container'>{filtered}</div>
 }
 
 export default FilteredPage

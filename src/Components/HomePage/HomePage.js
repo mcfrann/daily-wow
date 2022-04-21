80e2c5b2b966ecf1b43f269a3c53587f20a67337
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-21 16:49:52 -0600

    Put header in homepage component

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,30 +2,33 @@ import './HomePage.css'
 
 const HomePage = ({ todaysWow, error }) => {
   return (
-    <div className='about-text-container'>
-      {todaysWow !== [] && !error ? (
-        <>
-          <p className='about-text'>
-            Today's wow was featured in the movie {todaysWow.movie}, directed by{' '}
-            {todaysWow.director} in {todaysWow.year}. Owen Wilson plays{' '}
-            {todaysWow.character}, a character who says 'wow' a total of{' '}
-            {todaysWow.total_wows_in_movie} times throughout the entire duration
-            of the film. This particular 'wow' occurs at {todaysWow.timestamp}{' '}
-            of the movie.
-            <br />
-            <br />
-            Full line: "{todaysWow.full_line}"
-            <br />
-            <br />
-            Click on Owen to hear it!
-          </p>
-        </>
-      ) : todaysWow === [] && error ? (
-        <p className='about-text'>{error}</p>
-      ) : (
-        <p className='about-text'>Loading...</p>
-      )}
-    </div>
+    <>
+      <h1 className='about-header'>Your Daily Wow</h1>
+      <div className='about-text-container'>
+        {todaysWow !== [] && !error ? (
+          <>
+            <p className='about-text'>
+              Today's wow was featured in the movie {todaysWow.movie}, directed
+              by {todaysWow.director} in {todaysWow.year}. Owen Wilson plays{' '}
+              {todaysWow.character}, a character who says 'wow' a total of{' '}
+              {todaysWow.total_wows_in_movie} times throughout the entire
+              duration of the film. This particular 'wow' occurs at{' '}
+              {todaysWow.timestamp} of the movie.
+              <br />
+              <br />
+              Full line: "{todaysWow.full_line}"
+              <br />
+              <br />
+              Click on Owen to hear it!
+            </p>
+          </>
+        ) : todaysWow === [] && error ? (
+          <p className='about-text'>{error}</p>
+        ) : (
+          <p className='about-text'>Loading...</p>
+        )}
+      </div>
+    </>
   )
 }
 
